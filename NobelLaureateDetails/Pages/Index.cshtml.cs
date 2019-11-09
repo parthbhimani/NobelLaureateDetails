c137d37cdc00d487996b78a6f037186be5d7b7fa
parthbhimani <54642042+parthbhimani@users.noreply.github.com>
2019-11-09 18:48:22 -0500

    Add project files.

---
+++ NobelLaureateDetails/Pages/Index.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NobelLaureateDetails.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public JsonResult OnGet()
+        {
+            List<Laureate> filteredLaureates = new List<Laureate>();
+
+            using (WebClient webClient = new WebClient())
+            {
+                string PhysicsNinetyEight = webClient.DownloadString("http://api.nobelprize.org/v1/prize.json?category=physics&year=1998");
+                Prizes.Laureates Laureates = Prizes.Laureates.FromJson(PhysicsNinetyEight);
+
+                List<long> filteredIds = new List<long>();
+
+                foreach (Prizes.Prize prize in Laureates.PrizesArray)
+                {
+                    foreach (Prizes.Laureate laureate in prize.Laureates) {
+                        filteredIds.Add(laureate.Id);
+                    }
+                }
+
+                foreach (long id in filteredIds) {
+                    string laureateDetails = webClient.DownloadString("http://api.nobelprize.org/v1/laureate.json?id="+id);
+
+                    LaureateDetails array = LaureateDetails.FromJson(laureateDetails);
+
+                    foreach (Laureate laureate in array.Laureates) {
+                        filteredLaureates.Add(laureate);
+                    }
+                }
+
+                return new JsonResult(filteredLaureates);
+            }
+        }
+    }
+}
