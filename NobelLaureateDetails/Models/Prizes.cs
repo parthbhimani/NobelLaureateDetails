91605a2d4248d8d23fdbe199c968a8e5b71ac769
jainmt <54649290+jainmt@users.noreply.github.com>
2019-11-20 12:08:11 -0500

    Code Review jainmt

--- NobelLaureateDetails/Models/Prizes.cs
+++ NobelLaureateDetails/Models/Prizes.cs
@@ -10,7 +10,7 @@ namespace Prizes
 {
     using System;
     using System.Collections.Generic;
-
+    using System.ComponentModel.DataAnnotations;
     using System.Globalization;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Converters;
@@ -36,6 +36,7 @@ namespace Prizes
 
     public partial class Laureate
     {
+        [Required]
         [JsonProperty("id")]
         [JsonConverter(typeof(ParseStringConverter))]
         public long Id { get; set; }
