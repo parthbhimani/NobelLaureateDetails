472b9f72a315654e8e1dee0c9a5afea3e0f3d21b
Parth Doshi <doshipj@mail.uc.edu>
2019-12-02 16:55:09 -0500

    Consuming fellow student group JSON stream

---
+++ NobelLaureateDetails/Pages/foodSchedules.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using FoodSchedules;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NobelLaureateDetails.Pages
+{
+    public class foodSchedulesModel : PageModel
+    {
+        public void OnGet()
+        {
+            string output = getData("https://mobilefoodschedules.azurewebsites.net/api/SAApprovedFoodSchedules");
+            var schedules = Schedules.FromJson(output);
+            ViewData["schedules"] = schedules;
+
+        }
+
+        private string getData(string url)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return webClient.DownloadString(url);
+            }
+        }
+    }
+}
