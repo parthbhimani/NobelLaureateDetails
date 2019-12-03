6aed30fe4e526889c8827a0b97a7aa08b7d16192
parthbhimani <54642042+parthbhimani@users.noreply.github.com>
2019-12-03 17:49:20 -0500

    Final changes

---
+++ NobelLaureateDetails/Pages/JSONOutput.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NobelLaureateDetails.Pages
+{
+    public class JSONOutputModel : PageModel
+    {
+        private object webClient;
+
+        public void OnGet()
+        {
+        }
+
+
+    }
+}
